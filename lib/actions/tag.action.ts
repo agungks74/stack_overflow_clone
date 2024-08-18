bb8d8cc7e733525d597979796bbafa097b6de3cb
AgungKS <agungsyahferdi69@gmail.com>
2024-08-18 18:33:19 +0700

    done

--- lib/actions/tag.action.ts
+++ lib/actions/tag.action.ts
@@ -39,7 +39,7 @@ export async function getAllTags(params: GetAllTagsParams) {
   try {
     connectToDatabase();
 
-    const { searchQuery } = params;
+    const { searchQuery, filter } = params;
 
     const query: FilterQuery<typeof Tag> = {};
 
@@ -51,7 +51,26 @@ export async function getAllTags(params: GetAllTagsParams) {
       ];
     }
 
-    const tags = await Tag.find(query);
+    let sortOptions = {};
+
+    switch (filter) {
+      case "popular":
+        sortOptions = { questions: -1 };
+        break;
+      case "recent":
+        sortOptions = { createdAt: -1 };
+        break;
+      case "name":
+        sortOptions = { name: 1 };
+        break;
+      case "old":
+        sortOptions = { createdAt: 1 };
+        break;
+      default:
+        break;
+    }
+
+    const tags = await Tag.find(query).sort(sortOptions);
 
     return { tags };
   } catch (error) {
